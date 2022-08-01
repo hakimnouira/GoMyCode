0433c90f7ffc44eaa2abab134835458be0395d7c
hakimnouira <96267151+hakimnouira@users.noreply.github.com>
2022-08-01 15:57:05 +0100

    Add files via upload

---
+++ check point JSX And React Components Checkpoint/project/src/App.js
@@ -0,0 +1,24 @@
+import Profile from "./Component/Profile/ProfilePhoto"
+import Name from "./Component/Profile/FullName"
+import Adress from "./Component/Profile/Address"
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Profile />
+        <br />
+        <Name />
+        <Adress />
+ 
+        <p>
+         
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ check point JSX And React Components Checkpoint/project/src/Component/Profile/ProfilePhoto.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+
+
+
+export default class App extends Component {
+    constructor(props) {
+      super(props);
+    }
+
+    render () {
+
+
+
+
+   return (    <div>
+    
+    <img src="https://wallpaperaccess.com/full/235372.jpg" alt="." />
+    
+    
+     </div> )
+
+
+
+    }
+}
